93c98632d73310dd24e89949ab1232696e99e86c
Curtis Salter <XExtremeTechnologyX@GMAIL.COM>
2021-05-23 22:34:53 -0400

    Update op_params.py

--- common/op_params.py
+++ common/op_params.py
@@ -117,7 +117,7 @@ class opParams:
                         'steer_actuator_delay': Param(0.5, VT.number, 'The steer actuator delay', live=True),
                         'steer_up_15': Param(False, bool, 'Increase rate of steering up to 15, may fault on some cars'),
                         'traffic_light_alerts': Param(True, bool, "Switch off the traffic light alerts"),
-                        'traffic_lights': Param(False, bool, "Should Openpilot stop for traffic lights"),
+                        'traffic_lights': Param(True, bool, "Should Openpilot stop for traffic lights"),
                         'traffic_lights_without_direction': Param(False, bool, "Should Openpilot stop for traffic lights without a direction specified"),
                         'use_car_caching': Param(True, bool, 'Whether to use fingerprint caching'),
                         'min_TR': Param(None, VT.none_or_number, 'The minimum allowed following distance in seconds. Default is 0.9 seconds.\n'
